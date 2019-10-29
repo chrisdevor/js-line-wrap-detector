dab16432b69cc2922ab1b7320b7ee467ce23456f
chrisdevor <chrisdevor@macroplant.com>
2019-10-29 12:24:35 -0400

    Update lineWrapDetector.js

--- src/lineWrapDetector.js
+++ src/lineWrapDetector.js
@@ -9,7 +9,7 @@ var wrapWords = function(text, before, after, join) {
 
 var wrapWordsInChildElement = function(el) {
   if(el.nodeName == '#text') {
-    var words = el.textContent.split(/[\s↵]/g);
+    var words = el.innerText.split(/[\s↵]/g);
     for(var i=0;i<words.length;i++) {
       if(words[i].length > 0) {
         var span = document.createElement('span');
